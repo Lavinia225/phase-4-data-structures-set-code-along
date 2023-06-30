cd035fb8e4ddfe11b9f3585eb46bedbac7fd28b0
Luna Seifert <thepuddingdeity@gmail.com>
2023-06-30 12:22:28 -0500

    commit tree 06ce4ef091fab0270843fc4a28fc4ea5adad4e20

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,31 @@
 # your code here
+class MySet
+    def initialize enum = []
+        @hash = {}
+        enum.each do |value|
+            @hash[value] = true
+        end
+    end
+
+    def include? value
+            @hash.has_key? value
+    end
+
+    def add value
+        @hash[value] = true
+        self
+    end
+    
+    def delete value
+        @hash.delete value
+       self
+    end
+
+    def size
+        @hash.size
+    end
+
+    def each
+        @hash.each yield
+    end
+end
